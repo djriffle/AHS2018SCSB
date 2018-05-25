9081d1638a179adc878d287c981bada47999232a
Dylan Riffle <djriffle1@gmail.com>
2018-05-25 15:07:31 -0400

    Time Fix

--- ui.js
+++ ui.js
@@ -3,7 +3,11 @@ var time;
 var ti;
 var red = 0;
 var blue = 0;
-var off = true;
+var seconds;
+var minutes;
+var secondsDistance;
+var minutesDistance;
+
 let ui = {
     timerStart: document.getElementById('timerStart'),
     timer: document.getElementById('timer'),
@@ -29,6 +33,11 @@ let ui = {
   redScore.innerText = red;
     });
 
+  var offListener = firebase.database().ref('off'); 
+  offListener.on('value', function(snapshot) {
+  off = snapshot.val();
+    });
+
   var blueListener = firebase.database().ref('blue'); 
   blueListener.on('value', function(snapshot) {
   blue = snapshot.val();
@@ -44,45 +53,81 @@ let ui = {
 ui.timerStart.onclick = function () {
     if(off)
     {
-    time = 120
+
+    var startSound = new Audio('startSound.mp3');
+    startSound.play();
+    firebase.database().ref('off').set(false);
+    off = false
+    var now = new Date().getTime();
+    seconds = Math.floor((now % (1000 * 60)) / 1000);
+    minutes = Math.floor((now % (1000 * 60 * 60)) / (1000 * 60));
+    secondsDistance = seconds;
+    minutesDistance =  minutes + 2;
+    
     ui.timer.innerText = time;
     timeDestroyer();
     }
+  
 }
 
 var timeDestroyer = function(){
+    var now = new Date().getTime();
+    seconds = Math.floor((now % (1000 * 60)) / 1000);
+    minutes = Math.floor((now % (1000 * 60 * 60)) / (1000 * 60));
+    seconds = secondsDistance - seconds;
+    minutes = minutesDistance - minutes;
     
-    if(time == 0){
+    if(seconds < 0)
+    {
+      seconds += 60;
+      minutes--;
+    }
+
+    if(seconds <1 && minutes <1){
     	ui.timer.style.color = "red";
-        ui.timer.innerText = "0";
+      ui.timer.innerText = "0:00";
+      clearTimeout(ti);
     }
     else {
-    	
-    	time= time - 1;
-        firebase.database().ref('time').set(time);
+      if(seconds < 10)
+      {
+      time = minutes +":0" + seconds;
+      }
+      else
+      {
+      time = minutes +":" + seconds;
+      }
+      firebase.database().ref('time').set(time);
     	ui.timer.innerText = time;
-        ti = setTimeout(timeDestroyer, 1000); // check again in a second
+      ti = setTimeout(timeDestroyer, 1000); // check again in a second
     }
 }
 
 ui.reset.onclick = function () {
+    firebase.database().ref('off').set(true);
+    off = true;
     clearTimeout(ti);
-    time = 120;
-    firebase.database().ref('time').set('120');
+    firebase.database().ref('time').set('2:00');
     firebase.database().ref('red').set('0');
     firebase.database().ref('blue').set('0');
 }
 
 document.addEventListener('keydown', function(event) {
     if(event.keyCode == 13) {
+        if(red < 999)
+        {
         red++;
         firebase.database().ref('red').set(red);
         ui.redScore.innerText = red;
+        }
     }
     else if(event.keyCode == 16) {
+        if(blue < 999)
+        {
         blue++;
         firebase.database().ref('blue').set(blue);
         ui.blueScore.innerText = blue;
+        }
 
     }
 }
